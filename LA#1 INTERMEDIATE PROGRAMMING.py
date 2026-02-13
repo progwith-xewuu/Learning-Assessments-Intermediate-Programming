d2da1ff61e3031d4b00496d3b0bd59cf2a4732a4
Zylk Mandolado <allenph19@gmail.com>
2026-02-13 23:08:42 +0800

    Delete LA#1 INTERMEDIATE PROGRAMMING.py

--- LA#1 INTERMEDIATE PROGRAMMING.py
+++
@@ -1,4 +0,0 @@
-years = (int(input("Enter number of year/s: ")))
-converter = years*365*24*60*60
-# 365 days in 1 year 24 hours in 1 day 60 minutes in 1 hour 60 seconds in 1 minute
-print(f"{years} years in seconds is {converter}.")
