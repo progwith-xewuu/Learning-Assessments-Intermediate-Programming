960c38c6add146c6b9540ad6de2409d3200a9141
Zylk Mandolado <allenph19@gmail.com>
2026-02-13 15:38:12 +0800

    Add files via upload

---
+++ temperature_check.py
@@ -0,0 +1,8 @@
+temp = int(input("Current Temperature: "))
+
+if temp > 30:
+    print("Hot Day! Stay Hyrdated. ")
+else:
+    print("Pleasant Weather!")
+if temp < 10:
+    print("It's too Cold!") 
